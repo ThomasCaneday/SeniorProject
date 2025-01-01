37a71dc60462030d18228c0293fe92d312724d71
Thomas Caneday <110267727+ThomasCaneday@users.noreply.github.com>
2025-01-01 14:18:05 -0800

    Create send_metadata_and_length function
    Added function to send metadata info using the sendall function and the length of the metadata bytes

--- send_files_helpers.py
+++ send_files_helpers.py
@@ -13,3 +13,7 @@ def get_metadata_bytes(file_name):
     metadata = get_metadata(file_name, file_size)
     metadata_bytes = metadata.encode()
     return metadata_bytes
+
+def send_metadata_and_length(client_socket, metadata_bytes):
+    client_socket.sendall(struct.pack(">I", len(metadata_bytes))) # 4-byte length prefix
+    client_socket.sendall(metadata_bytes)
